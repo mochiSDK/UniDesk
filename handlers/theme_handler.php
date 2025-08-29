f6b58b134edec421689dbcc800a5535b20bd948b
Sofia Cipollone <sofia.cipollone5@gmail.com>
2025-08-30 00:02:04 +0200

    Fixed wrong navigation history after changing theme

--- handlers/theme_handler.php
+++ handlers/theme_handler.php
@@ -2,6 +2,13 @@
 // Save theme.
 if (isset($_POST['theme'])) {
     $_SESSION['theme'] = $_POST['theme'] === 'dark' ? 'dark' : 'light';
+    // This clears the navigation history from the theme POST request by building the correct redirect url.
+    $redirectUrl = $_SERVER['PHP_SELF'];
+    if (!empty($_SERVER['QUERY_STRING'])) {
+        $redirectUrl .= '?' . $_SERVER['QUERY_STRING'];
+    }
+    header("Location: $redirectUrl");
+    exit;
 }
 
 // Setting current theme (default to light).
