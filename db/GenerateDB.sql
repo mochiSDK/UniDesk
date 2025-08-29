562e96f83c1ac4d5bfbd86f3e29f3b76f64fcf28
Sofia Cipollone <sofia.cipollone5@gmail.com>
2025-08-30 00:19:18 +0200

    Added better ids in db script

--- db/GenerateDB.sql
+++ db/GenerateDB.sql
@@ -13,50 +13,50 @@ values ("mario.rossi@unibo.it", "mario.rossi", "$2y$10$eC02nvRYoRZT1VVhdKEmgO698
        ("caterina.marchesi@studio.unibo.it", "caterina.marchesi", "$2y$10$2gzZDAhw/j/lsYTKZhWR3OtzyhCGvbu0.mNPIPWx.TxE75HXnj0fS", false);
 
 insert into PRODUCT_CATEGORIES
-values ("C0", "Writing", "images/categories/writing.jpg"),
-       ("C1", "Stationery", "images/categories/stationery.jpg"),
-       ("C2", "Notebooks", "images/categories/notebook.jpg"),
-       ("C3", "Paper", "images/categories/paper.jpg"),
-       ("C4", "Binder", "images/categories/binder.jpg"),
-       ("C5", "Planner", "images/categories/planner.jpg");
+values ("C-68b223d070559", "Writing", "images/categories/writing.jpg"),
+       ("C-68b223eb46b47", "Stationery", "images/categories/stationery.jpg"),
+       ("C-68b22400b2072", "Notebooks", "images/categories/notebook.jpg"),
+       ("C-68b22411ce3c6", "Paper", "images/categories/paper.jpg"),
+       ("C-68b22421eb22b", "Binder", "images/categories/binder.jpg"),
+       ("C-68b2242cddd42", "Planner", "images/categories/planner.jpg");
 
 insert into PRODUCTS
-values ("P0", "C0", "Pigment liner 308", "Staedtler", 2.9, 10, "Blistercard containing 1 pigment liner black, line width approx. 0.2 mm", null, null, null, "images/products/PigmLin308.jpg"),
-       ("P1", "C0", "Textsurfer classic 364", "Staedtler", 4.9, 7, "Highlighter with large ink reservoir for extra-long highlighting performance", null, null, null, "images/products/Textsurfer364.jpg"),
-       ("P2", "C0", "Elance 421 45", "Staedtler", 1.9, 12, "Retractable ballpoint pen, longevity due to metal barrel and interchangable refill", null, null, null, "images/products/Elance421_45.jpg"),
-       ("P3", "C0", "Noris 120", "Staedtler", 1.5, 10, "Perfect graphite pencil in hexagonal shape - Made from Upcycled Wood", null, null, null, "images/products/Noris120.jpg"),
-       ("P4", "C0", "Lumocolor correctable 305", "Staedtler", 1.9, 7, "Non-permanent dry erase pen, for write-on films and other film surfaces", 15, 2, 5, "images/products/LumocolorCorrectable305.jpg"),
-       ("P5", "C0", "Tradition jumbo 1285", "Staedtler", 2.9, 8, "Perfect graphite pencil in triangular jumbo shape - Made from Upcycled Wood.", null, null, null, "images/products/TraditionJumbo1285.jpg"),
-       ("P6", "C0", "BIC Cristal Black Ballpoint", "BIC", 2.5, 6, "The classic BIC Cristal Original is the world's best-selling ballpoint pen and is available here in black ink", null, null, null, "images/products/BIC_CristalBlackBallpoint.jpg"),
-       ("P7", "C0", "Pelikan Graphite Pencils", "Pelikan", 4.5, 6, "Set of 3 graphite pencils; made of wood from responsibly managed forests FSC non-toxic", null, null, null, "images/products/PelikanGraphitePencils.jpg"),
-       ("P8", "C0", "Pilot Pen Frixion", "Pilot", 3, 5, " Erasable roller-ball pen. Stainless steel tip, 0.4 mm, replaceable lead.", null, null, null, "images/products/PilotPenFrixion.jpg"),
-       ("P9", "C1", "Mars plastic 526", "Staedtler", 2.5, 12, "Eraser in premium quality, for first-class erasing performance.", 6.5, 1.2, 2.2, "images/products/MarsPlastic526.jpg"),
-       ("P10", "C1", "Staedtler 511", "Staedtler", 3.5, 6, "Tub sharpener, for standard-sized blacklead pencils up to 8.2 mm", null, null, null, "images/products/Staedtler511.jpg"),
-       ("P11", "C1", "Noris 965", "Staedtler", 4.9, 10, "Blistercard containing scissors with 17 cm blade. Stainless steel, rounded blades.", 25, null, 11, "images/products/Noris965.jpg"),
-       ("P12", "C1", "Mars 562 PB", "Staedtler", 5, 7, "Plastic bag containing 1 ruler, length 30 cm.", 15, null, null, "images/products/Mars562PB.jpg"),
-       ("P13", "C1", "FABER-CASTELL Rubber", "Faber Castell", 4.3, 6, "Plastic eraser for clean and soft erasing, in white colour. Erase without smudging.", null, null, null, "images/products/FABER-CASTELLRubber.jpg"),
-       ("P14", "C1", "MAPED Cosmic Scissors", "Maped", 5.3, 7, "A product that combines tradition and innovation, designed for both enthusiasts and professionals", 21.5, 1.2, 8.4, "images/products/MAPEDCosmicScissors.jpg"),
-       ("P15", "C1", "Westcott Aluminium Ruler", "Westcott", 3.9, 5, "Aluminium ruler with bar for better handling, scale in cm and inches, 30 cm.", 30, null, null, "images/products/WestcottAluminiumRuler.jpg"),
-       ("P16", "C1", "Casio Scientific Calculator", "Casio", 9.9, 5, " Battery Powered, Light Blue scientific calculator. Dual line LCD display, 12 digits per line.", 16.2, null, 7.7, "images/products/CasioScientificCalculator.jpg"),
-       ("P17", "C2", "Kangaro Notebook A6", "Kangaro", 2.9, 10, "Kraft notebook A6 96 sheets, 70 g, lined with elastic band and drawing ribbon.", 14.8, 0.1, 10.5, "images/products/KangaroNotebookA6.jpg");
+values ("P-68b224a74befa", "C-68b223d070559", "Pigment liner 308", "Staedtler", 2.9, 10, "Blistercard containing 1 pigment liner black, line width approx. 0.2 mm", null, null, null, "images/products/PigmLin308.jpg"),
+       ("P-68b224a74bf1c", "C-68b223d070559", "Textsurfer classic 364", "Staedtler", 4.9, 7, "Highlighter with large ink reservoir for extra-long highlighting performance", null, null, null, "images/products/Textsurfer364.jpg"),
+       ("P-68b224a74bf24", "C-68b223d070559", "Elance 421 45", "Staedtler", 1.9, 12, "Retractable ballpoint pen, longevity due to metal barrel and interchangable refill", null, null, null, "images/products/Elance421_45.jpg"),
+       ("P-68b224a74bf28", "C-68b223d070559", "Noris 120", "Staedtler", 1.5, 10, "Perfect graphite pencil in hexagonal shape - Made from Upcycled Wood", null, null, null, "images/products/Noris120.jpg"),
+       ("P-68b224a74bf2a", "C-68b223d070559", "Lumocolor correctable 305", "Staedtler", 1.9, 7, "Non-permanent dry erase pen, for write-on films and other film surfaces", 15, 2, 5, "images/products/LumocolorCorrectable305.jpg"),
+       ("P-68b224a74bf2c", "C-68b223d070559", "Tradition jumbo 1285", "Staedtler", 2.9, 8, "Perfect graphite pencil in triangular jumbo shape - Made from Upcycled Wood.", null, null, null, "images/products/TraditionJumbo1285.jpg"),
+       ("P-68b224a74bf32", "C-68b223d070559", "BIC Cristal Black Ballpoint", "BIC", 2.5, 6, "The classic BIC Cristal Original is the world's best-selling ballpoint pen and is available here in black ink", null, null, null, "images/products/BIC_CristalBlackBallpoint.jpg"),
+       ("P-68b224a74bf34", "C-68b223d070559", "Pelikan Graphite Pencils", "Pelikan", 4.5, 6, "Set of 3 graphite pencils; made of wood from responsibly managed forests FSC non-toxic", null, null, null, "images/products/PelikanGraphitePencils.jpg"),
+       ("P-68b224a74bf37", "C-68b223d070559", "Pilot Pen Frixion", "Pilot", 3, 5, " Erasable roller-ball pen. Stainless steel tip, 0.4 mm, replaceable lead.", null, null, null, "images/products/PilotPenFrixion.jpg"),
+       ("P-68b224a74bf39", "C-68b223eb46b47", "Mars plastic 526", "Staedtler", 2.5, 12, "Eraser in premium quality, for first-class erasing performance.", 6.5, 1.2, 2.2, "images/products/MarsPlastiC-68b2242cddd4226.jpg"),
+       ("P-68b224a74bf3b", "C-68b223eb46b47", "Staedtler 511", "Staedtler", 3.5, 6, "Tub sharpener, for standard-sized blacklead pencils up to 8.2 mm", null, null, null, "images/products/Staedtler511.jpg"),
+       ("P-68b224a74bf3e", "C-68b223eb46b47", "Noris 965", "Staedtler", 4.9, 10, "Blistercard containing scissors with 17 cm blade. Stainless steel, rounded blades.", 25, null, 11, "images/products/Noris965.jpg"),
+       ("P-68b224a74bf41", "C-68b223eb46b47", "Mars 562 PB", "Staedtler", 5, 7, "Plastic bag containing 1 ruler, length 30 cm.", 15, null, null, "images/products/Mars562PB.jpg"),
+       ("P-68b224a74bf43", "C-68b223eb46b47", "FABER-CASTELL Rubber", "Faber Castell", 4.3, 6, "Plastic eraser for clean and soft erasing, in white colour. Erase without smudging.", null, null, null, "images/products/FABER-CASTELLRubber.jpg"),
+       ("P-68b224a74bf46", "C-68b223eb46b47", "MAPED Cosmic Scissors", "Maped", 5.3, 7, "A product that combines tradition and innovation, designed for both enthusiasts and professionals", 21.5, 1.2, 8.4, "images/products/MAPEDCosmicScissors.jpg"),
+       ("P-68b224a74bf49", "C-68b223eb46b47", "Westcott Aluminium Ruler", "Westcott", 3.9, 5, "Aluminium ruler with bar for better handling, scale in cm and inches, 30 cm.", 30, null, null, "images/products/WestcottAluminiumRuler.jpg"),
+       ("P-68b224a74bf51", "C-68b223eb46b47", "Casio Scientific Calculator", "Casio", 9.9, 5, " Battery Powered, Light Blue scientific calculator. Dual line LCD display, 12 digits per line.", 16.2, null, 7.7, "images/products/CasioScientificCalculator.jpg"),
+       ("P-68b225e1745c1", "C-68b22400b2072", "Kangaro Notebook A6", "Kangaro", 2.9, 10, "Kraft notebook A6 96 sheets, 70 g, lined with elastic band and drawing ribbon.", 14.8, 0.1, 10.5, "images/products/KangaroNotebookA6.jpg");
 
 insert into PRODUCT_MODELS
-values ("Yellow", "P1"),
-       ("Green", "P1"),
-       ("Pink", "P1");
+values ("Yellow", "P-68b224a74bf1c"),
+       ("Green", "P-68b224a74bf1c"),
+       ("Pink", "P-68b224a74bf1c");
 
 insert into ONLINE_ORDERS
-values ("O0", "mario.rossi@unibo.it", 7.80, "Delivered", '2025-07-01', '2025-07-05'),
-       ("O1", "virginia.trevisan@studio.unibo.it", 4.80, "Pending", '2025-07-02', '2025-07-06'),
-       ("O2", "fabiola.pisani@studio.unibo.it", 3.40, "Shipped", '2025-07-03', '2025-07-07'),
-       ("O3", "giuseppe.fiorentini@studio.unibo.it", 7.80, "Pending", '2025-08-04', '2025-08-08');
+values ("O-68b22650d5fc8", "mario.rossi@unibo.it", 7.80, "Delivered", '2025-07-01', '2025-07-05'),
+       ("O-68b22650d5ff1", "virginia.trevisan@studio.unibo.it", 4.80, "Pending", '2025-07-02', '2025-07-06'),
+       ("O-68b22650d5ff3", "fabiola.pisani@studio.unibo.it", 3.40, "Shipped", '2025-07-03', '2025-07-07'),
+       ("O-68b22650d5ff5", "giuseppe.fiorentini@studio.unibo.it", 7.80, "Pending", '2025-08-04', '2025-08-08');
 
 insert into includes
-values ("O0", "P0"),
-       ("O0", "P1"),
-       ("O1", "P0"),
-       ("O1", "P2"),
-       ("O2", "P3"),
-       ("O2", "P4"),
-       ("O3", "P0"),
-       ("O3", "P1");
+values ("O-68b22650d5fc8", "P-68b224a74befa"),
+       ("O-68b22650d5fc8", "P-68b224a74bf1c"),
+       ("O-68b22650d5ff1", "P-68b224a74befa"),
+       ("O-68b22650d5ff1", "P-68b224a74bf24"),
+       ("O-68b22650d5ff3", "P-68b224a74bf28"),
+       ("O-68b22650d5ff3", "P-68b224a74bf2a"),
+       ("O-68b22650d5ff5", "P-68b224a74befa"),
+       ("O-68b22650d5ff5", "P-68b224a74bf1c");
