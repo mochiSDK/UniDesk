68fa8387676ae88b97fc84414cfaef4e8a34b1ca
Sofia Cipollone <sofia.cipollone5@gmail.com>
2025-08-30 22:38:02 +0200

    Changed amount of best sellers to get from db in home page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once("bootstrap.php");
 
 $templateParams["categories"] = $db_helper->getCategories();
-$templateParams["bestSellers"] = $db_helper->getBestSellers(10);
+$templateParams["bestSellers"] = $db_helper->getBestSellers(5);
 $templateParams["randomProducts"] = $db_helper->getRandomProducts(10);
 
 require("templates/home.php");
